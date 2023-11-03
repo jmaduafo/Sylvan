52971c3b7926e93b01f87ef265030adb328b2ab6
Jay Madu <jmaduafokwa@hotmail.com>
2023-11-03 22:56:22 +0100

    added search params for sort by filtering

--- src/components/Shop/Filter.js
+++ src/components/Shop/Filter.js
@@ -2,40 +2,76 @@ import React, { useState } from 'react'
 import PlusCircle from '../icons/PlusCircle'
 import { motion } from 'framer-motion'
 import { categories } from '../../utils/shopCategories'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
+
 
 const Filter = ({setSelectedCategory, selectedCategory }) => {
     const [isSortClick, setIsSortClick ] = useState(false)
+    const [ searchParams, setSearchParams ] = useSearchParams()
+
+    console.log(searchParams)
 
     const sortCategories = [
         {
             'sortCategory': 'A-Z',
-            'types': null
+            'types': null,
+            'sortParams': 
+                {
+                    'sort': 'name',
+                    'order': 'asc'
+                }
+            
 
         },
         {
             'sortCategory': 'Z-A',
-            'types': null
+            'types': null,
+            'sortParams': 
+                {
+                    'sort': 'name',
+                    'order': 'desc'
+                }
         },
         {
-            'sortCategory': 'Oldest to newest',
-            'types': null
+            'sortCategory': 'Oldest to Newest',
+            'types': null,
+            'sortParams': 
+                {
+                    'sort': 'date',
+                    'order': 'oldest'
+                }
         },
         {
             'sortCategory': 'Newest to Oldest',
-            'types': null
+            'types': null,
+            'sortParams': 
+                {
+                    'sort': 'date',
+                    'order': 'newest'
+                }
         },
         {
             'sortCategory': '$ to $$$',
-            'types': null
+            'types': null,
+            'sortParams': 
+                {
+                    'sort': 'price',
+                    'order': 'asc'
+                }
         },
         {
             'sortCategory': '$$$ to $',
-            'types': null
+            'types': null,
+            'sortParams': 
+                {
+                    'sort': 'price',
+                    'order': 'desc'
+                }
         },
         {
             'sortCategory': 'Material:',
-            'types': ['Wood', 'Iron', 'Suede', 'Ceramics', 'Leather']
+            'types': ['Wood', 'Iron', 'Suede', 'Ceramics', 'Leather'],
+            'sortParams': null
         }
     ]
 
@@ -46,7 +82,7 @@ const Filter = ({setSelectedCategory, selectedCategory }) => {
             <div className='flex flex-wrap gap-2 mt-6'>
                 {categories.map(category => {
                     return (
-                        <Link  key={category} to={`/shop/${category.toLowerCase()}`}><p onClick={() => setSelectedCategory(category)} className='uppercase text-[12px] font-light px-4 py-[3px] rounded-full bg-olive text-cream text-center cursor-pointer'>{category}</p></Link>
+                        <Link key={category} to={`/shop/${category.toLowerCase()}`}><p onClick={() => setSelectedCategory(category)} className='uppercase text-[12px] font-light px-4 py-[3px] rounded-full bg-olive text-cream text-center cursor-pointer'>{category}</p></Link>
                     )
                 })}
             </div>
@@ -62,13 +98,13 @@ const Filter = ({setSelectedCategory, selectedCategory }) => {
             <motion.div initial={{ opacity: isSortClick ? 0 : 1 }} animate={{ opacity: isSortClick ? 1 : 0 }} className={`mt-6 border-t-siennaOpaque border-t-[1px] ${isSortClick ? 'visible' : 'invisible'} ${isSortClick ? 'h-full' : 'h-0'}`}>
                 {sortCategories.map(sort => {
                     return (
-                        <div key={sort.sortCategory} className='border-b-siennaOpaque border-b-[1px] p-1 cursor-pointer group duration-[.4s] ease-in'>
+                        <div key={sort.sortCategory} onClick={() => sort.sortParams && setSearchParams(sort.sortParams)} className='border-b-siennaOpaque border-b-[1px] p-1 cursor-pointer group duration-[.4s] ease-in'>
                            <p className='text-[13px] text-[#743406d8] group-hover:text-sienna'>{sort.sortCategory}</p>
                            {sort.types ? 
                             <div className='mt-4 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-2 grid grid-cols-1 gap-1'>
                                     {sort.types.map(type => {
-                                    return <div key={type} className='p-1 duration-[.4s] text-center border-siennaOpaque border-[1px] hover:bg-[#9b4e17b2] group'>
-                                        <p className='text-[13px] duration-[.4s] text-[#743406d8] hover:text-cream '>{type}</p>
+                                    return <div key={type} onClick={() => setSearchParams({'material': type.toLowerCase()})} className={`${searchParams.get('material') === type.toLowerCase() ? 'bg-[#9b4e17b2]' : 'transparent'} p-1 duration-[.4s] text-center border-siennaOpaque border-[1px] hover:bg-[#9b4e17b2] group`}>
+                                        <p className={`${searchParams.get('material') === type.toLowerCase() ? 'text-cream' : 'text-[#743406d8]'} text-[13px] duration-[.4s]  hover:text-cream `}>{type}</p>
                                     </div>
                                     })}
                             </div>
