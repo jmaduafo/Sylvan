9f7d115fa86ef4d6265ec549a36dec41c2462ac5
Jay Madu <jmaduafokwa@hotmail.com>
2023-11-15 20:27:29 +0100

    finished styling about page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,54 +1,198 @@
-import React from 'react'
+import React, { useState } from "react";
+import ArrowRight from "../components/icons/ArrowRight";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const About = () => {
+  const images = [
+    {
+      image:
+        "https://res.cloudinary.com/dyxxn831a/image/upload/v1699979090/Sylvan/interior-design-with-logs-plants_dwfhhm.jpg",
+      description: "a white vase next to a barrel of logs",
+      category: "home decor",
+    },
+    {
+      image:
+        "https://res.cloudinary.com/dyxxn831a/image/upload/v1700072908/Sylvan/Say_Hello_To_These_Summer_Trends_nivqb4.jpg",
+      description: "a quaint brown armchair on steps",
+      category: "chairs",
+    },
+    {
+      image:
+        "https://res.cloudinary.com/dyxxn831a/image/upload/v1700072929/Sylvan/Modern_Arc_Floor_Lamp_with_Shelf_in_Black_Glass_Shade_Included_f8pasw.jpg",
+      description: "a lamp beside a gray couch",
+      category: "lamps",
+    },
+    {
+      image:
+        "https://res.cloudinary.com/dyxxn831a/image/upload/v1699286438/Sylvan/EJ_295_hwi2rx.jpg",
+      description: "a beige couch beside a potted plant and gold table lamp",
+      category: "sofas",
+    },
+  ];
+
+  const [categoryHover, setCategoryHover] = useState("");
+  const [x, setX] = useState();
+  const [y, setY] = useState();
+
+  const easing = [0.39, 0.41, 0.37, 0.87];
+
   return (
-    <div className='text-sienna'>
+    <div
+      className="text-sienna"
+      onMouseMove={(e) => {
+        setX(e.clientX);
+        setY(e.clientY);
+      }}
+    >
+      <motion.div
+        initial={{ opacity: categoryHover.length ? 0 : 1  }}
+        animate={{ opacity: categoryHover.length ? 1 : 0, x: x, y: y }}
+        transition={{ duration: 0.3, ease: easing }}
+        className={`${
+          categoryHover.length ? "visible" : "invisible"
+        } flex justify-center items-center fixed top-0 w-[120px] h-[120px] rounded-full bg-sienna text-cream border-sienna border-[1px]`}
+      >
+        <p className="text-[14px] text-center">{categoryHover}</p>
+      </motion.div>
       <section>
-        <div className='flex flex-col sm:flex-row sm:justify-between sm:items-end sm:gap-8'>
-          <div className='overflow-hidden basis-full sm:basis-[65%] object-cover object-bottom'>
-            <img className='w-[150%] h-[150%]' src='https://res.cloudinary.com/dyxxn831a/image/upload/v1699978914/Sylvan/landon10_qwlnwy.jpg' alt='burnt orange background with vase and rustic sink'/>
+        <div className="flex flex-col sm:flex-row sm:justify-between sm:items-end sm:gap-8">
+          <div className="overflow-hidden basis-full sm:basis-[65%] object-cover object-bottom">
+            <img
+              className="w-[150%] h-[150%]"
+              src="https://res.cloudinary.com/dyxxn831a/image/upload/v1699978914/Sylvan/landon10_qwlnwy.jpg"
+              alt="burnt orange background with vase and rustic sink"
+            />
           </div>
-          <div className='basis-full sm:basis-[35%] sm:pb-8'>
-            <p className='w-full text-[14px] sm:w-[70%] mb-8'>Sylvan, a haven for those who appreciate the intersection of elegance and aesthetics in home decor. Our furniture store is more than a showroom; it's a curated collection where every piece is a masterpiece. Sylvan holds a steadfast commitment to attention to detail, meticulously selecting and showcasing pieces that embody sophistication and timeless design.</p>
-            <p className='w-full text-[14px] sm:w-[70%]'>With a sense of responsibility to our clientele, we pride ourselves on presenting not just furniture, but an elevated lifestyle. Each item in our collection is a testament to our dedication to offering the very best in curated pieces, where every nuance is considered, ensuring that Sylvan is synonymous with the epitome of refined living.</p>
-          </div>
-        </div>
-      </section>
-      <section className='mt-[10rem]'>
-        <div className='sm:w-[70%] mx-auto flex flex-col-reverse sm:flex-row sm:items-start sm:gap-[5rem]'>
-          <div className='sm:basis-[50%]'>
-            <h2 className='font-serif text-[40px]'>Serenity & Craftsmanship</h2>
-            <div className='sm:pl-[8rem] mt-[3rem]'>
-              <p className='text-[14px] mb-8'>Other travelling salesmen live a life of luxury. For instance, whenever I go back to the guest house during the morning to copy out the contract. These gentlemen are always still sitting there eating.</p>
-              <p className='text-[14px]'>I ought to just try that with my boss; I'd get kicked out on the spot. Doing business like this takes much more effort than doing your own business at home.</p>
-            </div>
-          </div>
-          <div className='sm:basis-[50%] flex items-start gap-5'>
-            <div className='basis-[50%] pt-[10rem] h-auto object-cover object-bottom'>
-              <img className='w-full h-full' src='https://res.cloudinary.com/dyxxn831a/image/upload/v1700057167/Sylvan/landon31_co8msi.png'/>
-            </div>
-            <div className='basis-[50%] h-auto object-cover object-bottom'>
-              <img className='w-full h-full' src='https://res.cloudinary.com/dyxxn831a/image/upload/v1700057218/Sylvan/landon32_hkkvbc.jpg'/>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className='mt-[10rem]'>
-        <div className='sm:w-[60%] mx-auto'>
-          <div></div>
-          <div className='text-center'>
-            <h2 className='font-serif text-[40px]'>Our Team</h2>
-            <div className='flex justify-center items-center'>
-              <p className='sm:w-[70%] text-[14px]'>A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame. It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
-            </div>
+          <div className="basis-full sm:basis-[35%] sm:pb-8">
+            <p className="w-full text-[14px] sm:w-[70%] mb-8">
+              Sylvan, a haven for those who appreciate the intersection of
+              elegance and aesthetics in home decor. Our furniture store is more
+              than a showroom; it's a curated collection where every piece is a
+              masterpiece. Sylvan holds a steadfast commitment to attention to
+              detail, meticulously selecting and showcasing pieces that embody
+              sophistication and timeless design.
+            </p>
+            <p className="w-full text-[14px] sm:w-[70%]">
+              With a sense of responsibility to our clientele, we pride
+              ourselves on presenting not just furniture, but an elevated
+              lifestyle. Each item in our collection is a testament to our
+              dedication to offering the very best in curated pieces, where
+              every nuance is considered, ensuring that Sylvan is synonymous
+              with the epitome of refined living.
+            </p>
           </div>
         </div>
       </section>
-      <section>
-        <div></div>
+      <section className="mt-[10rem]">
+        <div className="sm:w-[70%] mx-auto flex flex-col-reverse sm:flex-row sm:items-start sm:gap-[5rem]">
+          <div className="sm:basis-[50%]">
+            <h2 className="font-serif text-[40px]">Serenity & Craftsmanship</h2>
+            <div className="sm:pl-[8rem] mt-[3rem]">
+              <p className="text-[14px] mb-8">
+                Other travelling salesmen live a life of luxury. For instance,
+                whenever I go back to the guest house during the morning to copy
+                out the contract. These gentlemen are always still sitting there
+                eating.
+              </p>
+              <p className="text-[14px]">
+                I ought to just try that with my boss; I'd get kicked out on the
+                spot. Doing business like this takes much more effort than doing
+                your own business at home.
+              </p>
+            </div>
+          </div>
+          <div className="sm:basis-[50%] flex items-start gap-5">
+            <div className="basis-[50%] pt-[10rem] h-auto object-cover object-bottom">
+              <img
+                className="w-full h-full"
+                src="https://res.cloudinary.com/dyxxn831a/image/upload/v1700057167/Sylvan/landon31_co8msi.png"
+              />
+            </div>
+            <div className="basis-[50%] h-auto object-cover object-bottom">
+              <img
+                className="w-full h-full"
+                src="https://res.cloudinary.com/dyxxn831a/image/upload/v1700057218/Sylvan/landon32_hkkvbc.jpg"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="mt-[10rem]">
+        <div className="sm:w-[55%] mx-auto">
+          <div className="flex items-center gap-[4rem]">
+            <div className="basis-[33.333%] object-cover object-bottom">
+              <img
+                className="w-full h-full"
+                src="https://res.cloudinary.com/dyxxn831a/image/upload/v1700057157/Sylvan/landon40_hm1fkx.jpg"
+                alt=""
+              />
+            </div>
+            <div className="basis-[33.333%] object-cover object-bottom">
+              <img
+                className="w-full h-full"
+                src="https://res.cloudinary.com/dyxxn831a/image/upload/v1700057174/Sylvan/landon42_zyrszr.jpg"
+                alt=""
+              />
+            </div>
+            <div className="basis-[33.333%] object-cover object-bottom">
+              <img
+                className="w-full h-full"
+                src="https://res.cloudinary.com/dyxxn831a/image/upload/v1700057164/Sylvan/landon41_phvyfp.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="text-center mt-[3rem]">
+            <h2 className="font-serif text-[40px]">Our Team</h2>
+            <div className="flex justify-center items-center">
+              <p className="sm:w-[75%] text-[14px]">
+                A collection of textile samples lay spread out on the table -
+                Samsa was a travelling salesman - and above it there hung a
+                picture that he had recently cut out of an illustrated magazine
+                and housed in a nice, gilded frame. It showed a lady fitted out
+                with a fur hat and fur boa who sat upright, raising a heavy fur
+                muff that covered the whole of her lower arm towards the viewer.
+                Gregor then turned to look out the window at the dull weather.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="mt-[10rem]">
+        <div className="w-[70%] mx-auto">
+          <p className="italic text-[14px]">featured categories</p>
+          <div className="mt-2 flex justify-between items-center gap-4 flex-wrap">
+            {images.map((img) => {
+              return (
+                <Link to={`/shop/${img.category}`} key={img}>
+                  <div
+                    onMouseEnter={() => setCategoryHover(img.category)}
+                    onMouseLeave={() => setCategoryHover("")}
+                    className="overflow-hidden w-[250px] h-[320px] object-cover object-bottom cursor-none"
+                  >
+                    <img
+                      className="w-full h-full rounded-tr-[50px] cursor-none"
+                      src={img.image}
+                      alt={img.description}
+                    />
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+          <Link to='/shop/all'>
+          <div className="group cursor-pointer mt-2 flex justify-end items-center gap-2">
+            <p className="group-hover:italic text-[14px]">shop all</p>
+            <div className="duration-[.4s] group-hover:transform group-hover:translate-x-1">
+              <ArrowRight />
+            </div>
+          </div>
+          </Link>
+        </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
