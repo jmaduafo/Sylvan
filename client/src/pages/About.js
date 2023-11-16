7826a0a9ca335a9bc118a7cba627cf3e9cbe93db
Jay Madu <jmaduafokwa@hotmail.com>
2023-11-16 11:29:43 +0100

    trying to fix server error

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
       image:
         "https://res.cloudinary.com/dyxxn831a/image/upload/v1699979090/Sylvan/interior-design-with-logs-plants_dwfhhm.jpg",
       description: "a white vase next to a barrel of logs",
-      category: "home decor",
+      category: "accents",
     },
     {
       image:
@@ -59,28 +59,28 @@ const About = () => {
         <div className="flex flex-col sm:flex-row sm:justify-between sm:items-end sm:gap-8">
           <div className="overflow-hidden basis-full sm:basis-[65%] object-cover object-bottom">
             <img
-              className="w-[150%] h-[150%]"
+              className="w-full h-full"
               src="https://res.cloudinary.com/dyxxn831a/image/upload/v1699978914/Sylvan/landon10_qwlnwy.jpg"
               alt="burnt orange background with vase and rustic sink"
             />
           </div>
           <div className="basis-full sm:basis-[35%] sm:pb-8">
-            <p className="w-full text-[14px] sm:w-[70%] mb-8">
+            <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: .6, delay: .8, ease: easing }} className="w-full text-[14px] sm:w-[70%] mb-8">
               Sylvan, a haven for those who appreciate the intersection of
               elegance and aesthetics in home decor. Our furniture store is more
               than a showroom; it's a curated collection where every piece is a
               masterpiece. Sylvan holds a steadfast commitment to attention to
               detail, meticulously selecting and showcasing pieces that embody
               sophistication and timeless design.
-            </p>
-            <p className="w-full text-[14px] sm:w-[70%]">
+            </motion.p>
+            <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: .6, delay: .8, ease: easing }} className="w-full text-[14px] sm:w-[70%]">
               With a sense of responsibility to our clientele, we pride
               ourselves on presenting not just furniture, but an elevated
               lifestyle. Each item in our collection is a testament to our
               dedication to offering the very best in curated pieces, where
               every nuance is considered, ensuring that Sylvan is synonymous
               with the epitome of refined living.
-            </p>
+            </motion.p>
           </div>
         </div>
       </section>
@@ -89,27 +89,27 @@ const About = () => {
           <div className="sm:basis-[50%]">
             <h2 className="font-serif text-[40px]">Serenity & Craftsmanship</h2>
             <div className="sm:pl-[8rem] mt-[3rem]">
-              <p className="text-[14px] mb-8">
+              <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: .6, delay: .8, ease: easing }} className="text-[14px] mb-8">
                 Other travelling salesmen live a life of luxury. For instance,
                 whenever I go back to the guest house during the morning to copy
                 out the contract. These gentlemen are always still sitting there
                 eating.
-              </p>
-              <p className="text-[14px]">
+              </motion.p>
+              <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: .6, delay: .8, ease: easing }} className="text-[14px]">
                 I ought to just try that with my boss; I'd get kicked out on the
                 spot. Doing business like this takes much more effort than doing
                 your own business at home.
-              </p>
+              </motion.p>
             </div>
           </div>
           <div className="sm:basis-[50%] flex items-start gap-5">
-            <div className="basis-[50%] pt-[10rem] h-auto object-cover object-bottom">
+            <div data-scroll data-scroll-speed='.1' className="basis-[50%] pt-[10rem] h-auto object-cover object-bottom">
               <img
                 className="w-full h-full"
                 src="https://res.cloudinary.com/dyxxn831a/image/upload/v1700057167/Sylvan/landon31_co8msi.png"
               />
             </div>
-            <div className="basis-[50%] h-auto object-cover object-bottom">
+            <div data-scroll data-scroll-speed='.3' className="basis-[50%] h-auto object-cover object-bottom">
               <img
                 className="w-full h-full"
                 src="https://res.cloudinary.com/dyxxn831a/image/upload/v1700057218/Sylvan/landon32_hkkvbc.jpg"
@@ -118,7 +118,7 @@ const About = () => {
           </div>
         </div>
       </section>
-      <section className="mt-[10rem]">
+      <motion.section initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: .6, delay: .8, ease: easing }} className="mt-[10rem]">
         <div className="sm:w-[55%] mx-auto">
           <div className="flex items-center gap-[4rem]">
             <div className="basis-[33.333%] object-cover object-bottom">
@@ -158,8 +158,8 @@ const About = () => {
             </div>
           </div>
         </div>
-      </section>
-      <section className="mt-[10rem]">
+      </motion.section>
+      <motion.section initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: .6, delay: .8, ease: easing }} className="mt-[10rem]">
         <div className="w-[70%] mx-auto">
           <p className="italic text-[14px]">featured categories</p>
           <div className="mt-2 flex justify-between items-center gap-4 flex-wrap">
@@ -190,7 +190,7 @@ const About = () => {
           </div>
           </Link>
         </div>
-      </section>
+      </motion.section>
     </div>
   );
 };
