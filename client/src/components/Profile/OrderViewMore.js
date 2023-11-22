8f9af15371cf3db9e560ed61cff250bd7a066dae
Jay Madu <jmaduafokwa@hotmail.com>
2023-11-22 14:47:54 +0100

    added data rendering for order view more page

--- client/src/components/Profile/OrderViewMore.js
+++ client/src/components/Profile/OrderViewMore.js
@@ -2,8 +2,10 @@ import React from 'react'
 import pic from '../../assets/hero-image.jpg'
 import LeftArrow from '../icons/LeftArrow'
 import { easeInOut, motion } from 'framer-motion'
+import { epoch } from '../../utils/epochConverter'
 
 const OrderViewMore = ({order, setViewMore, viewMore}) => {
+  console.log(order)
   return (
     <motion.div initial={{ opacity: viewMore ? 0 : 1 }} animate={{ opacity: viewMore ? 1 : 0 }} transition={{ duration: .5, ease: easeInOut}}>
       <div onClick={() => setViewMore(false)} className='group text-sienna flex items-center gap-2 mb-3 cursor-pointer pl-2 w-fit'>
@@ -23,34 +25,34 @@ const OrderViewMore = ({order, setViewMore, viewMore}) => {
           <p className='font-light text-[14px]'>Price</p>
         </div>
         <div className='basis-[20%]'>
-          <p className='font-light text-[14px]'>Date Ordered</p>
+          <p className='font-light text-[14px]'>Order ID</p>
         </div>
       </div>
-      {[1, 2, 3, 4, 5, 6, 7].map(order => {
+      {order?.items?.length && order?.items?.map(ordered => {
         return (
-          <div key={order} className='py-3 px-4 flex text-sienna items-center border-b-siennaOpaque border-b-[1px]'>
+          <div key={ordered.id} className='py-3 px-4 flex text-sienna items-center border-b-siennaOpaque border-b-[1px]'>
             <div className='basis-[45%] flex gap-2'>
               <div className='w-[90px] h-[120px] object-cover object-bottom'>
-                <img className='w-full h-full' src={pic} alt={order}/>
+                <img className='w-full h-full' src={ordered.images[0]} alt={ordered.images[0]}/>
               </div>
               <div className='py-2'>
-                <p className='uppercase font-light text-[13px]'>Lovelace Chaise</p>
+                <p className='uppercase font-light text-[13px]'>{ordered.name}</p>
                 <div className='flex gap-1 items-center'>
-                  <p className='text-[13px] font-light'>M</p>
+                  <p className='text-[13px] font-light'>{ordered.selectedSize}</p>
                   <div className='w-[12px] h-[12px] rounded-full border-sienna border-[1px] flex justify-center items-center'>
-                    <div className='w-[8px] h-[8px] rounded-full bg-olive'></div>
+                    <div className='w-[8px] h-[8px] rounded-full' style={{ backgroundColor: ordered.selectedColor}}></div>
                   </div>
                 </div>
               </div>
             </div>
             <div className='basis-[20%]'>
-              <p className='font-light text-[13px]'>2</p>
+              <p className='font-light text-[13px]'>{ordered.cartQuantity}</p>
             </div>
             <div className='basis-[15%]'>
-              <p className='font-light text-[13px]'>$5,892.45</p>
+              <p className='font-light text-[13px]'>${ordered.price?.toString().length > 6 ? Intl.NumberFormat().format(parseFloat(ordered.price).toFixed(2)) : parseFloat(ordered.price).toFixed(2)}</p>
             </div>
             <div className='basis-[20%]'>
-              <p className='text-[13px] font-light w-fit'>Oct. 13</p>
+              <p className='text-[13px] font-light w-fit'>#{order?.orderId && order?.orderId}</p>
             </div>
         </div>
         )
